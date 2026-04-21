fc896543fb12a7b6c27a118011c6303cc4db1b73
CS <kalin87@live.com>
2026-04-21 18:09:50 +0000

    Fix for JoyCaption

--- Captioning/JoyCaption/joy_caption_batch.py
+++ Captioning/JoyCaption/joy_caption_batch.py
@@ -17,11 +17,7 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 # Helps prevent memory fragmentation on cards with less VRAM
 os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
 
-# Old prompt
-# system_prompt = "Write a detailed description for this image in 50 words or less. Do NOT mention any text that is in the image."
-
-# New prompt
-system_prompt = "Write a detailed description for this image in 50 words or less. Do NOT mention any text that is in the image. CRITICAL INSTRUCTION: You must strictly avoid describing age, race, ethnicity, skin tone, or body shape. Do NOT use words like Latina, Hispanic, white, black, Asian, tan skin, olive skin, pale, curvy, young, old, adult, slim, or thick. Keep descriptions of the person strictly to their clothing, pose, and hair color."
+system_prompt = "Write a detailed description for this image in 100 words or less. Do NOT mention any text that is in the image. CRITICAL INSTRUCTION: You must strictly avoid describing age, race, ethnicity, skin tone, or body shape. Do NOT use words to describe race or ethnicity such as Latina, Hispanic, Asian, or any similar racial or national descriptors. You may describe facial expression, gaze direction, pose, clothing and hair in detail."
 
 NETWORK_VOLUME = os.getenv("NETWORK_VOLUME")
 
